ee0af89273e3fcff90b808adfd10552a2eee2fa7
nchp7 <133721544+nchp7@users.noreply.github.com>
2023-11-27 11:19:16 -0300

    i still need to work on this project

---
+++ script.js
@@ -0,0 +1,10 @@
+const gridContainer = document.getElementById("grid-container");
+
+  for (let i = 0; i < 16 * 16; i++) {
+    // Create a new div element for each grid square
+    const gridSquare = document.createElement("div");
+
+    // Add a class to the grid square for styling
+    gridSquare.classList.add("grid-square");
+
+}
